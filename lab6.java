f773d940686e775b5d886a8d2ac8a003c3f62e17
IbaiO <ibaiolaziregiehu@gmail.com>
2025-03-14 11:46:40 +0100

    import-ak sartuta

--- lab6.java
+++ lab6.java
@@ -1,3 +1,9 @@
+import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
 public class lab6 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
