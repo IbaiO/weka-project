1e3054fe62986e17b97d1eee35acbbc4c598e222
IbaiO <ibaiolaziregiehu@gmail.com>
2025-03-14 11:47:35 +0100

    Falta zena xd

--- lab6.java
+++ lab6.java
@@ -1,6 +1,7 @@
 import weka.filters.unsupervised.attribute.StringToWordVector;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.unsupervised.attribute.FixedDictionaryStringToWordVector;
 
 public class lab6 {
     public static void main(String[] args) {
