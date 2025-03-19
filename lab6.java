51914032457bcc565b7f4524f774bb838ae926b7
EnekoMa <enekoma13@gmail.com>
2025-03-19 11:02:13 +0100

    Pruebas de copilot

--- lab6.java
+++ lab6.java
@@ -1,7 +1,9 @@
+import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
-import weka.filters.unsupervised.attribute.FixedDictionaryStringToWordVector;
+import weka.core.converters.ArffSaver;
+import java.io.File;
 
 public class lab6 {
     public static void main(String[] args) {
@@ -17,6 +19,14 @@ public class lab6 {
             System.out.println("Mesedez, egiaztatu helbidea ondo sartu duzula.");
             return;
         }        
+
+        Instances bowData = transformToBagOfWords(data);
+        if (bowData == null) {
+            System.out.println("Errorea: Ezin izan da Bag of Words transformazioa burutu.");
+            return;
+        }
+
+        saveData(bowData, gordeleku);
     }
 
     private static Instances loadData(String filePath) { // Oso totxo, igual ez dira behar horrenbeste if
@@ -45,5 +55,41 @@ public class lab6 {
 
         System.out.println("Class index set to: " + data.classIndex());
         return data;
+    }
+
+    private static Instances transformToBagOfWords(Instances data) {
+        StringToWordVector filter = new StringToWordVector();
+        filter.setLowerCaseTokens(true); // Convertir a minúsculas
+        filter.setOutputWordCounts(true); // Contar palabras
+        filter.setTFTransform(true); // Transformación TF
+        filter.setIDFTransform(true); // Transformación IDF
+        filter.setAttributeIndices("first-last"); // Aplicar a todos los atributos
+        filter.setWordsToKeep(1000); // Número de palabras a mantener
+        filter.setDoNotOperateOnPerClassBasis(true); // No operar por clase
+        filter.setStemmer(null); // No usar stemmer
+        filter.setTokenizer(new weka.core.tokenizers.WordTokenizer()); // Tokenizador por defecto
+
+        try {
+            filter.setInputFormat(data);
+            Instances newData = Filter.useFilter(data, filter);
+            return newData;
+        } catch (Exception e) {
+            System.out.println("ERROREA: Ezin izan da Bag of Words transformazioa burutu.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void saveData(Instances data, String filePath) {
+        ArffSaver saver = new ArffSaver();
+        saver.setInstances(data);
+        try {
+            saver.setFile(new File(filePath));
+            saver.writeBatch();
+            System.out.println("Datuak gorde dira: " + filePath);
+        } catch (Exception e) {
+            System.out.println("ERROREA: Ezin izan da " + filePath + " fitxategia gorde.");
+            e.printStackTrace();
+        }
     }
 }
