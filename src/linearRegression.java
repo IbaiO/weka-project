f3dbc82f75e2e7c7b45a29f80e33c64c903efb69
IbaiO <ibaiolaziregiehu@gmail.com>
2025-03-26 10:22:08 +0100

    linear regression sortua (ez dabil)

---
+++ src/linearRegression.java
@@ -0,0 +1,28 @@
+package src;
+
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.classifiers.functions.LinearRegression;
+
+public class linearRegression{
+	public static void main(String args[]) throws Exception{
+		//Load Data set
+		DataSource source = new DataSource("D:\\DataScienceCollection\\Weka\\house.arff");
+		Instances dataset = source.getDataSet();
+		//set class index to the last attribute
+		dataset.setClassIndex(dataset.numAttributes()-1);
+		
+		//Build model
+		LinearRegression model = new LinearRegression();
+		model.buildClassifier(dataset);
+		//output model
+		System.out.println("LR FORMULA : "+model);	
+		
+		// Now Predicting the cost 
+		Instance myHouse = dataset.lastInstance();
+		double price = model.classifyInstance(myHouse);
+		System.out.println("-------------------------");	
+		System.out.println("PRECTING THE PRICE : "+price);	
+	}
+}
