142e075fbb03057fbd614721d9306f2de2396cb4
IbaiO <ibaiolaziregiehu@gmail.com>
2025-03-26 10:51:41 +0100

    orain badoa linear regression + lib

--- src/linearRegression.java
+++ src/linearRegression.java
@@ -5,24 +5,57 @@ import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.functions.LinearRegression;
 
-public class linearRegression{
-	public static void main(String args[]) throws Exception{
-		//Load Data set
-		DataSource source = new DataSource("/home/ibai/GitHub/weka-project/laborategiak/Datuak-20250314/1_ToyStringExample/toyStringExample_train_BoW.arff");
-		Instances dataset = source.getDataSet();
-		//set class index to the last attribute
-		dataset.setClassIndex(dataset.numAttributes()-1);
-		
-		//Build model
-		LinearRegression model = new LinearRegression();
-		model.buildClassifier(dataset);
-		//output model
-		System.out.println("LR FORMULA : "+model);	
-		
-		// Now Predicting the cost 
-		Instance myHouse = dataset.lastInstance();
-		double price = model.classifyInstance(myHouse);
-		System.out.println("-------------------------");	
-		System.out.println("PRECTING THE PRICE : "+price);	
-	}
+public class linearRegression {
+    public static void main(String[] args) {
+        String dataSource = "/home/ibai/GitHub/weka-project/probaData/toyStringExample_NonSparseBoW.arff";
+
+        Instances dataset = loadData(dataSource);
+        if (dataset == null) {
+            System.out.println("Mesedez, egiaztatu helbidea ondo sartu duzula.");
+            return;
+        }
+
+        dataset.setClassIndex(dataset.numAttributes() - 1);
+
+        LinearRegression model = buildModel(dataset);
+        if (model != null) {
+            System.out.println("LR FORMULA : " + model);
+            predictPrice(model, dataset);
+        }
+    }
+
+    private static Instances loadData(String dataSource) {
+        try {
+            DataSource source = new DataSource(dataSource);
+            return source.getDataSet();
+        } catch (Exception e) {
+            System.out.println("ERROREA: Ezin izan da datu multzoa kargatu.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static LinearRegression buildModel(Instances dataset) {
+        try {
+            LinearRegression model = new LinearRegression();
+            model.buildClassifier(dataset);
+            return model;
+        } catch (Exception e) {
+            System.out.println("ERROREA: Ezin izan da modeloa eraiki.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void predictPrice(LinearRegression model, Instances dataset) {
+        try {
+            Instance myHouse = dataset.lastInstance();
+            double price = model.classifyInstance(myHouse);
+            System.out.println("-------------------------");
+            System.out.println("PRECTING THE PRICE : " + price);
+        } catch (Exception e) {
+            System.out.println("ERROREA: Ezin izan da prezioa aurreikusi.");
+            e.printStackTrace();
+        }
+    }
 }
