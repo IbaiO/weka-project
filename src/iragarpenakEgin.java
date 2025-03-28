af65de78c4bb2196b5e6b3d39f65aee54a769209
IbaiO <ibaiolaziregiehu@gmail.com>
2025-03-28 11:51:22 +0100

    Iragarpena .txt batean gorde

--- src/iragarpenakEgin.java
+++ src/iragarpenakEgin.java
@@ -4,6 +4,10 @@ import weka.classifiers.functions.LinearRegression;
 import weka.core.Instance;
 import weka.core.Instances;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class iragarpenakEgin {
     private static void main(Instances dataset) {
         LinearRegression modelLR = linearRegression.linearRegressionSortu(null);
@@ -13,18 +17,21 @@ public class iragarpenakEgin {
     }
     
     private static void linearRegressionIragarri(LinearRegression model, Instances dataset) {
-        try {
+        String outputFilePath = "/home/ibai/GitHub/weka-project/probaData/iragarpena_LinearRegression.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
             for (int i = 0; i < dataset.numInstances(); i++) {
                 Instance instance = dataset.instance(i);
                 double prediction = model.classifyInstance(instance);
                 String predictedClass = prediction > 0.5 ? "Pos" : "Neg";
-                System.out.println("Predicted class for instance " + (i + 1) + ": " + predictedClass);
+                writer.write("Instance " + (i + 1) + ": Predicted Class = " + predictedClass + "\n");
             }
+            System.out.println("Predictions saved to: " + outputFilePath);
+        } catch (IOException e) {
+            System.out.println("ERROREA: Ezin izan da iragarpenak fitxategian gorde.");
+            e.printStackTrace();
         } catch (Exception e) {
             System.out.println("ERROREA: Ezin izan da iragarpenik egin.");
             e.printStackTrace();
         }
     }
-    
-    //hemen ipintzeko iragarpenak egiteko klasea baina orokortua, en vez de bakoitza bere classifier klasean
 }
