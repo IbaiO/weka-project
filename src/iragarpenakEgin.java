7e94d698f6b12e81214f647029de986cb98a7624
IbaiO <ibaiolaziregiehu@gmail.com>
2025-03-28 11:42:31 +0100

    Iragarpen tokia aldatuta

--- src/iragarpenakEgin.java
+++ src/iragarpenakEgin.java
@@ -1,6 +1,30 @@
 package src;
 
+import weka.classifiers.functions.LinearRegression;
+import weka.core.Instance;
+import weka.core.Instances;
+
 public class iragarpenakEgin {
+    private static void main(Instances dataset) {
+        LinearRegression modelLR = linearRegression.linearRegressionSortu(null);
+        if (modelLR != null) {
+            linearRegressionIragarri(modelLR, dataset);
+        }
+    }
+    
+    private static void linearRegressionIragarri(LinearRegression model, Instances dataset) {
+        try {
+            for (int i = 0; i < dataset.numInstances(); i++) {
+                Instance instance = dataset.instance(i);
+                double prediction = model.classifyInstance(instance);
+                String predictedClass = prediction > 0.5 ? "Pos" : "Neg";
+                System.out.println("Predicted class for instance " + (i + 1) + ": " + predictedClass);
+            }
+        } catch (Exception e) {
+            System.out.println("ERROREA: Ezin izan da iragarpenik egin.");
+            e.printStackTrace();
+        }
+    }
     
     //hemen ipintzeko iragarpenak egiteko klasea baina orokortua, en vez de bakoitza bere classifier klasean
 }
--- src/linearRegression.java
+++ src/linearRegression.java
@@ -1,26 +1,25 @@
 package src;
 
-import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.functions.LinearRegression;
 
 public class linearRegression {
-    public static void main(String[] args) {
+    public static LinearRegression linearRegressionSortu(String[] args) {
         String dataSource = "/home/ibai/GitHub/weka-project/probaData/toyStringExample_NonSparseBoW.arff";
 
         Instances dataset = loadData(dataSource);
         if (dataset == null) {
             System.out.println("Mesedez, egiaztatu helbidea ondo sartu duzula.");
-            return;
+            return null;
         }
 
         dataset.setClassIndex(dataset.numAttributes() - 1);
 
         LinearRegression model = buildModel(dataset);
-        if (model != null) {
-            predictClass(model, dataset);
-        }
+        if (model != null)
+            return model;
+        return null;
     }
 
     private static Instances loadData(String dataSource) {
@@ -43,19 +42,6 @@ public class linearRegression {
             System.out.println("ERROREA: Ezin izan da modeloa eraiki.");
             e.printStackTrace();
             return null;
-        }
-    }
-
-    private static void predictClass(LinearRegression model, Instances dataset) {
-        try {
-            Instance myHouse = dataset.lastInstance();
-            double prediction = model.classifyInstance(myHouse);
-            String predictedClass = prediction > 0.5 ? "Pos" : "Neg";
-            System.out.println("-------------------------");
-            System.out.println("PREDICTED CLASS: " + predictedClass);
-        } catch (Exception e) {
-            System.out.println("ERROREA: Ezin izan da klasea aurreikusi.");
-            e.printStackTrace();
         }
     }
 }
