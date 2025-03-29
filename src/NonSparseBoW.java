799151be8a469443f4ccce1b63bb16c9566d651a
EnekoMa <enekoma13@gmail.com>
2025-03-29 19:20:37 +0100

    Horain ondo eratzen da BoWNonSparse (gordetzen da baita ere)

--- src/NonSparseBoW.java
+++ src/NonSparseBoW.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.util.regex.Pattern;
+import java.io.File;
 
 import weka.attributeSelection.AttributeSelection;
 import weka.attributeSelection.BestFirst;
@@ -10,6 +11,7 @@ import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 import weka.filters.unsupervised.instance.SparseToNonSparse;
+
 
 @SuppressWarnings("all")
 public class NonSparseBoW {
@@ -21,11 +23,13 @@ public class NonSparseBoW {
         } return nireNonSparseBoW;
     }
 
-    public Instances transform(Instances data) {
+    public Instances transform(Instances data, String outFile) {
         Instances dataGarbi = datu_garbiketa(data);
         Instances BoWData = transformToBoW(data);
         Instances NonSparseBoWData = transformToBoWNonSparse(BoWData);
-        return NonSparseBoWData;
+        Instances filteredData = datu_garbiketa2(NonSparseBoWData);
+        save(filteredData, outFile);
+        return filteredData;
     }
 
     private Instances datu_garbiketa(Instances datuak) {
@@ -46,6 +50,10 @@ public class NonSparseBoW {
             }
         }
 
+        return datuak;
+    }
+
+    private Instances datu_garbiketa2(Instances datuak) {
         // Use the AttributeSelection class to perform feature selection
         AttributeSelection attributeSelection = new AttributeSelection();
         CfsSubsetEval evaluator = new CfsSubsetEval();
@@ -106,6 +114,22 @@ public class NonSparseBoW {
             System.out.println("ERROREA: Ezin izan da Non Sparse transformazioa burutu.");
             e.printStackTrace();
             return null;
+        }
+    }
+
+    private void save(Instances data, String outFile) {
+        try {
+            // .model luzapena kendu eta BoW.arff bihurtu
+            String arffOutFile = outFile.replaceAll("\\.model$", "BoW.arff");
+            // Gorde datuak .arff formatuan
+            weka.core.converters.ArffSaver saver = new weka.core.converters.ArffSaver();
+            saver.setInstances(data);
+            saver.setFile(new File(arffOutFile));
+            saver.writeBatch();
+            System.out.println("Transformed data saved to: " + arffOutFile);
+        } catch (Exception e) {
+            System.out.println("ERROREA: Ezin izan da BoW datuak gorde.");
+            e.printStackTrace();
         }
     }
 }
