e1ccd59a6b027e569b84c4664449344036b458d3
IbaiO <ibaiolaziregiehu@gmail.com>
2025-04-06 00:49:18 +0200

    Nahasmen matrizeak + 5cfv

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import weka.core.Attribute;
+import weka.core.DenseInstance;
 import weka.core.Instances;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+
+import java.util.Random;
 
 public class Main {
     
@@ -62,11 +68,36 @@ public class Main {
                 accuracyKalkulatu(predictionFilePaths, instances[2]);
             }
         }
+
+        // 5-fold cross-validation
+        Instances dataset = datuBilketa.getDB().bildu(args[0], args[1])[0]; // Example: train dataset
+
+        // 5-fold cross-validation: SMO PolyKernel
+        weka.classifiers.functions.SMO smoPolyKernel = new weka.classifiers.functions.SMO();
+        smoPolyKernel.setKernel(new weka.classifiers.functions.supportVector.PolyKernel());
+        performCrossValidation(smoPolyKernel, dataset, 5, "src/emaitzak/smo_poly_kernel_5cfv.txt");
+
+        // 5-fold cross-validation: SMO RBFKernel
+        weka.classifiers.functions.SMO smoRBFKernel = new weka.classifiers.functions.SMO();
+        smoRBFKernel.setKernel(new weka.classifiers.
+        functions.supportVector.RBFKernel());
+        performCrossValidation(smoRBFKernel, dataset, 5, "src/emaitzak/smo_rbf_kernel_5cfvs.txt");
+
+        // 5-fold cross-validation: SMO PukKernel
+        weka.classifiers.functions.SMO smoPukKernel = new weka.classifiers.functions.SMO();
+        smoPukKernel.setKernel(new weka.classifiers.functions.supportVector.Puk());
+        performCrossValidation(smoPukKernel, dataset, 5, "src/emaitzak/smo_puk_kernel_5cfvs.txt");
+
+        // 5-fold cross-validation: Linear Regression
+        dataset = preprocessDataLR(dataset);
+        dataset.deleteWithMissingClass(); // Eliminar instancias con valores faltantes en el atributo de clase
+        Classifier linearRegression = new weka.classifiers.functions.LinearRegression();
+        performCrossValidationWithMetrics(linearRegression, dataset, 5, "src/emaitzak/linear_regression_5cfvs.txt");
     }
 
     public static void accuracyKalkulatu(String[] predictionFilePaths, Instances devSet) {
         try {
-            // Create a table to store the results
+            // Emaitzak gordetzeko taula sortu
             List<String[]> table = new ArrayList<>();
             table.add(new String[]{"True Value", "Linear Regression", "SMO Poly Kernel", "SMO RBF Kernel", "SMO Puk Kernel"});
 
@@ -80,6 +111,12 @@ public class Main {
             // Accuracy kalkulatzeko aldagaiak
             int[] correctPredictions = new int[4];
             int totalInstances = devSet.numInstances();
+
+            // Nahasmen matrizeak sortu
+            int[][] linearRegressionMatrix = new int[2][2];
+            int[][] smoPolyKernelMatrix = new int[2][2];
+            int[][] smoRBFKernelMatrix = new int[2][2];
+            int[][] smoPukKernelMatrix = new int[2][2];
 
             // Dev multzoko instantziak iteratu
             for (int i = 0; i < totalInstances; i++) {
@@ -95,12 +132,24 @@ public class Main {
                 if (trueValue.equalsIgnoreCase(smoRBFKernelPrediction)) correctPredictions[2]++;
                 if (trueValue.equalsIgnoreCase(smoPukKernelPrediction)) correctPredictions[3]++;
 
+                // Nahasmen matrizeen balioak jarri
+                updateConfusionMatrix(linearRegressionMatrix, trueValue, linearRegressionPrediction);
+                updateConfusionMatrix(smoPolyKernelMatrix, trueValue, smoPolyKernelPrediction);
+                updateConfusionMatrix(smoRBFKernelMatrix, trueValue, smoRBFKernelPrediction);
+                updateConfusionMatrix(smoPukKernelMatrix, trueValue, smoPukKernelPrediction);
+
                 // Taularen errenkada gehitu
                 table.add(new String[]{trueValue, linearRegressionPrediction, smoPolyKernelPrediction, smoRBFKernelPrediction, smoPukKernelPrediction});
             }
 
             // Taula fitxategi batean gorde
             saveTableToFile(table, "src/emaitzak/accuracy_table.txt");
+
+            // Nahasmen matrizeak gorde
+            saveConfusionMatrixToFile(linearRegressionMatrix, "Linear Regression", "src/emaitzak/linear_regression_confusion_matrix.txt");
+            saveConfusionMatrixToFile(smoPolyKernelMatrix, "SMO Poly Kernel", "src/emaitzak/smo_poly_kernel_confusion_matrix.txt");
+            saveConfusionMatrixToFile(smoRBFKernelMatrix, "SMO RBF Kernel", "src/emaitzak/smo_rbf_kernel_confusion_matrix.txt");
+            saveConfusionMatrixToFile(smoPukKernelMatrix, "SMO Puk Kernel", "src/emaitzak/smo_puk_kernel_confusion_matrix.txt");
 
             // Accuracy kalkulatu eta erakutsi
             System.out.println("Accuracy Results:");
@@ -113,6 +162,22 @@ public class Main {
         } catch (Exception e) {
             System.out.println("ERROREA: Ezin izan da accuracy taula sortu.");
             e.printStackTrace();
+        }
+    }
+
+    private static void updateConfusionMatrix(int[][] matrix, String trueValue, String predictedValue) {
+        int trueIndex = trueValue.equalsIgnoreCase("pos") ? 0 : 1;
+        int predictedIndex = predictedValue.equalsIgnoreCase("pos") ? 0 : 1;
+        matrix[trueIndex][predictedIndex]++;
+    }
+
+    private static void saveConfusionMatrixToFile(int[][] matrix, String modelName, String filePath) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write("Confusion Matrix for " + modelName + ":\n");
+            writer.write("\t\tPredicted: pos\tPredicted: neg\n");
+            writer.write("True: pos\t" + matrix[0][0] + "\t\t" + matrix[0][1] + "\n");
+            writer.write("True: neg\t" + matrix[1][0] + "\t\t" + matrix[1][1] + "\n");
+            writer.write("-----------------------------------\n");
         }
     }
 
@@ -148,6 +213,144 @@ public class Main {
                 writer.write(String.join("\t", row));
                 writer.newLine();
             }
+        }
+    }
+
+    public static void performCrossValidation(Classifier model, Instances dataset, int folds, String outputFilePath) throws Exception {
+        dataset.setClassIndex(0);
+        dataset.deleteWithMissingClass();
+
+        // Ebaluzio objektua sortu
+        Evaluation evaluation = new Evaluation(dataset);
+
+        // 5-fold cross-validation exekutatu
+        evaluation.crossValidateModel(model, dataset, folds, new Random(1));
+
+        // Emaitzak gorde
+        StringBuilder results = new StringBuilder();
+        results.append("=== Cross-validation Results ===\n");
+        results.append("Model: ").append(model.getClass().getSimpleName()).append("\n");
+        results.append("Accuracy: ").append(String.format("%.2f", evaluation.pctCorrect())).append("%\n");
+        results.append("Precision: ").append(String.format("%.2f", evaluation.weightedPrecision())).append("\n");
+        results.append("Recall: ").append(String.format("%.2f", evaluation.weightedRecall())).append("\n");
+        results.append("F1 Score: ").append(String.format("%.2f", evaluation.weightedFMeasure())).append("\n");
+        results.append(evaluation.toMatrixString("Confusion Matrix")).append("\n");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+            writer.write(results.toString());
+        }
+    }
+
+    public static void performCrossValidationWithMetrics(Classifier model, Instances dataset, int folds, String outputFilePath) throws Exception {
+        // Linear Regression-erako berezia
+        dataset.setClassIndex(0);
+
+        // Ebaluzio objektua sortu
+        Evaluation evaluation = new Evaluation(dataset);
+
+        // Ejecutar 5-fold cross-validation
+        evaluation.crossValidateModel(model, dataset, folds, new Random(1));
+
+        // Kalkuluetarako aldagaiak
+        int truePositives = 0;
+        int falsePositives = 0;
+        int falseNegatives = 0;
+        int trueNegatives = 0;
+        for (int i = 0; i < dataset.numInstances(); i++) {
+            double trueValue = dataset.instance(i).classValue();
+            double predictedValue = evaluation.predictions().get(i).predicted();
+
+            String predictedClass = predictedValue >= 0.5 ? "pos" : "neg";
+            String trueClass = trueValue == 1.0 ? "pos" : "neg";
+
+            if (trueClass.equals("pos")) {
+                if (predictedClass.equals("pos")) {
+                    truePositives++;
+                } else {
+                    falseNegatives++;
+                }
+            } else if (trueClass.equals("neg")) {
+                if (predictedClass.equals("neg")) {
+                    trueNegatives++;
+                } else {
+                    falsePositives++;
+                }
+            }
+        }
+
+        // Balioak kalkulatu
+        double precision = (double) truePositives / (truePositives + falsePositives);
+        double recall = (double) truePositives / (truePositives + falseNegatives);
+        double f1Score = 2 * ((precision * recall) / (precision + recall));
+        double accuracy = (double) (truePositives + trueNegatives) / dataset.numInstances();
+
+        // Emaitzak gorde
+        StringBuilder results = new StringBuilder();
+        results.append("=== Cross-validation Results ===\n");
+        results.append("Model: ").append(model.getClass().getSimpleName()).append("\n");
+        results.append("Accuracy: ").append(String.format("%.2f", accuracy * 100)).append("%\n");
+        results.append("Precision: ").append(String.format("%.4f", precision)).append("\n");
+        results.append("Recall: ").append(String.format("%.4f", recall)).append("\n");
+        results.append("F1 Score: ").append(String.format("%.4f", f1Score)).append("\n");
+        results.append("Confusion Matrix\n");
+        results.append("   a   b   <-- classified as\n");
+        results.append(String.format("  %3d %3d |   a = pos\n", truePositives, falseNegatives));
+        results.append(String.format("  %3d %3d |   b = neg\n", falsePositives, trueNegatives));
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+            writer.write(results.toString());
+        }
+    }
+
+    private static Instances preprocessDataLR(Instances dataset) {
+        try {
+            dataset.setClassIndex(0);
+            int classIndex = dataset.classIndex();
+
+            if (dataset.attribute(classIndex).isNominal() && dataset.attribute(classIndex).numValues() == 2) {
+                ArrayList<Attribute> attributes = new ArrayList<>();
+                for (int i = 0; i < dataset.numAttributes(); i++) {
+                    if (i == classIndex) {
+                        attributes.add(new Attribute("class"));
+                    } else {
+                        attributes.add(dataset.attribute(i));
+                    }
+                }
+
+                Instances newDataset = new Instances(dataset.relationName(), attributes, dataset.numInstances());
+                newDataset.setClassIndex(classIndex);
+
+                // Convertir las instancias
+                for (int i = 0; i < dataset.numInstances(); i++) {
+                    DenseInstance newInstance = new DenseInstance(newDataset.numAttributes());
+                    newInstance.setDataset(newDataset);
+
+                    for (int j = 0; j < dataset.numAttributes(); j++) {
+                        if (j == classIndex) {
+                            String classValue = dataset.instance(i).stringValue(classIndex);
+                            if (classValue.equalsIgnoreCase("pos")) {
+                                newInstance.setValue(classIndex, 1.0);
+                            } else if (classValue.equalsIgnoreCase("neg")) {
+                                newInstance.setValue(classIndex, 0.0);
+                            } else {
+                                newInstance.setMissing(classIndex);
+                            }
+                        } else {
+                            newInstance.setValue(j, dataset.instance(i).value(j));
+                        }
+                    }
+
+                    newDataset.add(newInstance);
+                }
+
+                return newDataset;
+            }
+
+            return dataset;
+        } catch (Exception e) {
+            System.out.println("Error al procesar los datos para Linear Regression.");
+            e.printStackTrace();
+            return null;
         }
     }
 }
